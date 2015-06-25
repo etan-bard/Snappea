33401f5200a5b6137d932985f141d772fc598a2c
Etan Bard <ebard@pugetsound.edu>
2015-06-24 18:18:24 -0700

    Structured packages for test and main. Created stub class.

---
+++ SnappeaSort/src/main/java/com/snappeasort/SnappeaSorter.java
@@ -0,0 +1,6 @@
+package com.snappeasort;
+
+public class SnappeaSorter 
+{
+
+}
---
+++ SnappeaSort/src/test/java/com/testworks/Main.java
@@ -0,0 +1,9 @@
+package com.testworks;
+
+public class Main {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+	}
+}
